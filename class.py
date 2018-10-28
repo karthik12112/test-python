821c8ca0fa672c96279a33238d963b1e8125726b
karthik <35927737+karthik12112@users.noreply.github.com>
2018-10-28 01:17:16 -0700

    Create class.py

---
+++ class.py
@@ -0,0 +1,10 @@
+class asa():
+    def ssm_install(self, test):
+        print ("{} printing1").format(test)
+    def abc(self):
+        print("printing 2")
+
+karthik="hhh"
+if __name__ == '__main__':
+    asa().ssm_install(test=karthik)
+    asa().abc()
